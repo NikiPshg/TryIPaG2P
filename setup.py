2abe7797ce5566ada6c038cf8aa963b1ffc831b1
NikiPshg <nicitacom2017@gmail.com>
2025-03-01 21:49:18 +0300

    setuo

--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as f:
-    long_description = f.read()
+try:
+    with open("README.md", "r", encoding="utf-8") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = "TryRuIpa: A package for grapheme to phoneme."
 
-with open("requirements.txt", "r") as f:
-    requirements = f.read().splitlines()
+try:
+    with open("requirements.txt", "r", encoding="utf-8") as f:
+        requirements = f.read().splitlines()
+except FileNotFoundError:
+    requirements = []
 
 setup(
     name="TryRuIpa",
     version="0.1.0",
     description="TryRuIpa: A package for grapheme to phoneme.",
     long_description=long_description,
-    long_description_content_type="text/markdown", 
+    long_description_content_type="text/markdown",  
     author="NikiPshg",
     url="https://github.com/NikiPshg/TryIPaG2P",
     license="MIT",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"}, 
+    packages=find_packages(where="src")
+    package_dir={"": "src"},
     python_requires=">=3.8",
-    install_requires=requirements, 
-    include_package_data=True, 
+    install_requires=requirements,  
+    include_package_data=True,
 )
